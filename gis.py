d2ba1dc6b336125431aad7b3d8715df955d12a07
Yigit Saritabak <ge35kok@mytum.de>
2023-11-18 16:12:02 +0100

    water level

--- gis.py
+++ gis.py
@@ -6,33 +6,17 @@ from mayavi import mlab
 # Load the GeoTIFF data using rasterio
 geotiff_path = f'C:/Users/yigit/Desktop/UNI/RCI/1.Semester/hackatum/bldy/data/test2.tif'
 with rasterio.open(geotiff_path) as src:
-    elevation = src.read(1)  # Assuming the elevation data is in the first band
-
-    # Get the geotransform information to calculate coordinates
-    #transform = src.transform
-    #x_size = src.width
-    #y_size = src.height
-
-    # Create coordinate grids
-    #x, y = np.meshgrid(np.linspace(transform[2], transform[2] + transform[0] * (x_size - 1), x_size),
-                      #np.linspace(transform[5], transform[5] + transform[4] * (y_size - 1), y_size))
-
-
+    elevation = src.read(1) 
+    
 nrows,ncols=elevation.shape
 x,y=np.meshgrid(np.arange(ncols),np.arange(nrows))
 z=elevation
-mesh=mlab.mesh(x,y,z)
-# Create a Mayavi figure
-#fig = mlab.figure()
 
-# Create a surface plot of the topography
-#surf = mlab.surf(x, y, elevation, colormap='terrain')
+z_test=z
+z_test[z_test<=505]=505
 
-# Add contours to the surface plot
-#contour = mlab.contour_surf(x, y, elevation, contours=10, colormap='terrain')
-
-# Add a color bar to the figure
-#mlab.colorbar(orientation='vertical')
+mesh=mlab.mesh(x,y,z_test,colormap='terrain')
+desired_elevation = 510
 
 # Show the plot
 mlab.show()
